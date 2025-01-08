e4a72a053c675ea6c63a481d065376f3b020e933
Gabriel Moura <gabrielmoura-cma@hotmail.com>
2025-01-07 21:42:35 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import api from "./api";
+import githubLogo from "./images/github.svg";
+
+function App() {
+  const [userName, setUserName] = useState("");
+  const [userData, setUserData] = useState();
+
+  const loadUserData = async () => {
+    await api.get(userName).then((response) => {
+      setUserData(response.data);
+      console.log(response.data);
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    loadUserData();
+  };
+
+  return (
+    <>
+      <main>
+        <div className="form_wrapper">
+          <img src={githubLogo} alt="Github Logo" width={48} />
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              value={userName}
+              onChange={(e) => setUserName(e.target.value)}
+              placeholder="Search for any profile on github"
+            />
+          </form>
+        </div>
+        {userData ? (
+          // <UserCard data={data}/>
+          <div className="user_card">
+            <img
+              src={userData.avatar_url}
+              alt="User avatar"
+              className="user_avatar"
+            />
+            <div className="user_details">
+              <span>Name:{userData.name}</span>
+              <span>followers:{userData.followers}</span>
+              <span>following:{userData.following}</span>
+              <span>repos:{userData.public_repos}</span>
+            </div>
+          </div>
+        ) : (
+          <></>
+        )}
+      </main>
+    </>
+  );
+}
+
+export default App;
